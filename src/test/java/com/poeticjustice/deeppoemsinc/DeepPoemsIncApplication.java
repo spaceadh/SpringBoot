ef0e9716b91e1ced7e33c665d851282b1c226d39
spaceadh <spacesealadhesives13@gmail.com>
2024-08-29 16:55:30 +0300

    TestCheck

--- src/test/java/com/poeticjustice/deeppoemsinc/DeepPoemsIncApplication.java
+++ src/test/java/com/poeticjustice/deeppoemsinc/DeepPoemsIncApplication.java
@@ -1,11 +1,22 @@
 package com.poeticjustice.deeppoemsinc;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
-public class DeepPoemsIncApplication {
+public class DeepPoemsIncApplication implements CommandLineRunner {
+
+	public static final Logger logger = LoggerFactory.getLogger(DeepPoemsIncApplication.class);
 	public static void main(String[] args) {
+		logger.info("Starting application");
 		SpringApplication.run(DeepPoemsIncApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Application started");
 	}
 }
